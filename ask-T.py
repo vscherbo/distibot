9fdfb3b810e73030ff483b6d7c7f3b2300272efc
Vladimir Scherbo <vscherbo@gmail.com>
2015-05-12 16:16:13 +0300

    changed log filename format; changed loop condition

--- ask-T.py
+++ ask-T.py
@@ -7,21 +7,26 @@ import sys
 import time
 
 def signal_handler(signal, frame):
- 	log.close()
-        sys.exit(0)
+    loop_flag = False
+ 	// log.close()
+    // sys.exit(0)
+
 signal.signal(signal.SIGINT, signal_handler)
 
 print("Create sensor")
 sensor = W1ThermSensor()
 print("Sensor is created")
 
-log = open('output.txt','w')
-while True:
+log = open('sensor-'+time.strftime("%Y-%m-%d-%H-%M") +'.csv','w')
+loop_flag = True
+while loop_flag:
     temperature_in_celsius = sensor.get_temperature()
     print(time.strftime("%H:%M:%S")+ ","+ str(temperature_in_celsius), file=log)
     print(time.strftime("%H:%M:%S")+ ","+ str(temperature_in_celsius))
     sleep(2)
 
+log.close()
+sys.exit(0)
 
 # sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "0000066c6502")
 # ll /sys/bus/w1/devices/
