fd1379a61d606babb89b5e1bdf17073b16a0b7ea
Vladimir Scherbo <vscherbo@gmail.com>
2015-12-23 23:03:47 +0300

    added timer_show

--- bottle/make_table.tpl
+++ bottle/make_table.tpl
@@ -1,5 +1,4 @@
 %#template to generate a HTML table from a list of tuples (or list of lists, or tuple of tuples or ...)
-<meta http-equiv="refresh" content="5">
 <p>The open items are as follows({{timestamp}}):</p>
 <link type="text/css" href="main.css" rel="stylesheet" >
 <table border="1">
--- bottle/todo.py
+++ bottle/todo.py
@@ -3,11 +3,10 @@ from datetime import datetime
 from bottle import route, run, debug, template, static_file, request, get
 
 # Static Routes
-"""
 @get('/<filename:re:.*\.js>')
 def javascripts(filename):
         return static_file(filename, root='static/js')
-"""
+
 @get('/<filename:re:.*\.css>')
 def stylesheets(filename):
     return static_file(filename, root='static/css')
@@ -23,6 +22,15 @@ def todo_list():
     output = template('make_table', rows=result, timestamp=datetime.now())
     return output
     # return str(result)
+
+@route('/timer')
+def timer_show():
+    output = template('timer_show')
+    return output
+
+@route('/ask_timer')
+def ask_timer():
+    return str(datetime.now())
 
 #add this at the very end:
 debug(True)
