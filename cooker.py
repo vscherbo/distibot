a5fcf2785bd1b15e4374a15ed502d4dbba4b888a
Vladimir Scherbo <vscherbo@gmail.com>
2016-03-19 11:18:39 +0300

    version 0.1a

--- cooker.py
+++ cooker.py
@@ -5,8 +5,10 @@ import RPIO
 import time
 
 class Cooker:
-    power_max = 2000
-    power_min = 120
+    powers = (120, 300, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000)
+    max_power_index = len(powers)-1
+    power_max = powers[-1]
+    power_min = powers[0]
     def __init__(self, gpio_on_off, gpio_up, gpio_down):
         self.gpio_on_off = gpio_on_off
         RPIO.setup(self.gpio_on_off, RPIO.OUT, initial=RPIO.HIGH)
@@ -14,13 +16,12 @@ class Cooker:
         RPIO.setup(self.gpio_up, RPIO.OUT, initial=RPIO.HIGH)
         self.gpio_down = gpio_down
         RPIO.setup(self.gpio_down, RPIO.OUT, initial=RPIO.HIGH)
-        self.power = 1400
+        self.power_index = 6 # 1400W
         self.state_on = False
     def click_button(self,gpio_port_num):
         RPIO.output(gpio_port_num, 0)
         time.sleep(0.1)
         RPIO.output(gpio_port_num, 1)
-
     def switch_on(self):
         if not self.state_on:
             self.click_button(self.gpio_on_off)
@@ -30,11 +31,18 @@ class Cooker:
             self.click_button(self.gpio_on_off)
             self.state_on = False
     def power_up(self):
-        if self.power < power_max:
+        if self.power_index < max_power_index:
             self.click_button(self.gpio_up)
-            self.power += 200
+            self.power_index += 1
+            return True
+        else:
+            return False
     def power_down(self):
-        if self.power > power_min:
+        if self.power_index > 0:
             self.click_button(self.gpio_down)
-            self.power -= 200
-
+            self.power_index -= 1
+            return True
+        else:
+            return False
+    def current_power(self):
+        return self.powers[self.power_index]
