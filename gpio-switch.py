16627d616e956ab5f965173221aabee5786ea9b8
Vladimir Scherbo <vscherbo@gmail.com>
2016-04-25 21:22:36 +0300

    test interrupts

--- gpio-switch.py
+++ gpio-switch.py
@@ -2,17 +2,38 @@
 # -*- coding: utf-8 -*-
 
 from time import sleep
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+import RPIO
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+RPIO.setwarnings(False)
+RPIO.setmode(RPIO.BCM)
 
+"""
 ngpio=17
-#GPIO.cleanup() 
 GPIO.setup(ngpio, GPIO.OUT)     #конфигурируем GPIO 7 как выход
 GPIO.output(ngpio, True)               #выводим на GPIO 7 логическую "1" (3.3 V)
 sleep(1)
 GPIO.output(ngpio, False)              #выводим на GPIO 7 логический "0"
 sleep(2)
 GPIO.output(ngpio, True)               #выводим на GPIO 7 логическую "1" (3.3 V)
-GPIO.cleanup() 
+"""
+
+def gpio_callback(gpio_id, val):
+    print("gpio %s: %s" % (gpio_id, val))
+
+ngpio=14
+
+RPIO.setup(ngpio, RPIO.IN, pull_up_down=RPIO.PUD_DOWN)
+RPIO.add_interrupt_callback(ngpio, gpio_callback, debounce_timeout_ms=100)
+#        , threaded_callback=True)
+#, pull_up_down=RPIO.PUD_UP)
+
+RPIO.wait_for_interrupts(threaded=True)
+
+n = 0
+while True:
+    sleep(1)
+    n += 1
+    print n
+
+RPIO.cleanup() 
