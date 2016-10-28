a31189c4e9c3b051ab49db9457ef2fe028330b6b
Vladimir Scherbo <vscherbo@gmail.com>
2016-10-28 17:18:21 +0300

    изменение глобальной переменной в потоке

--- demo-threads.py
+++ demo-threads.py
@@ -5,12 +5,16 @@ import time
 
 # Define a function for the thread
 def print_time( threadName, delay):
-   count = 0
-   while count < 5:
-      time.sleep(delay)
-      count += 1
-      print "%s: %s" % ( threadName, time.ctime(time.time()) )
+    global var1
+    count = 0
+    while count < 5:
+        time.sleep(delay)
+        count += 1
+        print "%s: %s, var1=%s" % ( threadName, time.ctime(time.time()), var1 )
+        if "Thread-1" == threadName:
+            var1 = count + 1
 
+var1 = 0;
 # Create two threads as follows
 try:
    thread.start_new_thread( print_time, ("Thread-1", 2, ) )
