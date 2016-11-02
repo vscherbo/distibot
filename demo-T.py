3d65d5d34a58dac0efaae063966983aa3c5e279b
Vladimir Scherbo <vscherbo@gmail.com>
2016-11-02 20:35:22 +0300

    pep8 recomendations

--- demo-T.py
+++ demo-T.py
@@ -5,10 +5,11 @@ from __future__ import print_function
 from w1thermsensor import W1ThermSensor
 from time import sleep
 from pushbullet import Pushbullet
-import signal 
+import signal
 import sys
 import time
 import thread
+
 
 def signal_handler(signal, frame):
     global loop_flag
@@ -16,15 +17,17 @@ def signal_handler(signal, frame):
     # log.close()
     # sys.exit(0)
 
+
 def temperature_loop(Talarms):
-    global loop_flag 
+    global loop_flag
     global log
     global temperature_in_celsius
     Talarm = Talarms.pop(0)
     alarm_cnt = 0
     while loop_flag:
         temperature_in_celsius = sensor.get_temperature()
-        print(time.strftime("%H:%M:%S")+ ","+ str(temperature_in_celsius), file=log)
+        print("%s%s", time.strftime("%H:%M:%S"), temperature_in_celsius,
+              sep=',', file=log)
         if temperature_in_celsius > Talarm:
             c.push_note("Превысили "+str(Talarm), str(temperature_in_celsius))
             alarm_cnt += 1
@@ -37,33 +40,24 @@ signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGHUP, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
-# Talarms = [25.0, 25.5, 25.9, 999.9] # debug
-# Talarms = [77.0, 79.0, 85.0, 88.0, 94.5, 98.5, 999.9] # 1st production
-Talarms = [94.5, 98.7, 999.9] # tails
+# Talarms = [25.0, 25.5, 25.9, 999.9]  # debug
+# Talarms = [77.0, 79.0, 85.0, 88.0, 94.5, 98.5, 999.9]  # 1st production
+Talarms = [94.5, 98.7, 999.9]  # tails
 alarm_limit = 3
 
 pb = Pushbullet('XmJ61j9LVdjbPyKcSOUYv1k053raCeJP')
-# one_plus_one = pb.get_device('OnePlus One')
 c = [x for x in pb.channels if x.name == u"Billy's moonshine"][0]
-
-
-# Title, Message_body
-# to device
-# push = one_plus_one.push_note("Процесс", "Тело.")
-# push = pb.push_note("Hello world!", "We're using the api.", device=one_plus_one)
-# to channel
-# c.push_note("Hello "+ c.name, "Hello My Channel")
-
 
 print("Create sensor")
 sensor = W1ThermSensor()
 print("Sensor is created")
-log = open('sensor-'+time.strftime("%Y-%m-%d-%H-%M") +'.csv','w', 0) # 0 - unbuffered write
+# 0 - unbuffered write
+log = open('sensor-' + time.strftime("%Y-%m-%d-%H-%M") + '.csv', 'w', 0)
 loop_flag = True
 temperature_in_celsius = 0
 
 try:
-    thread.start_new_thread( temperature_loop, (Talarms,) )
+    thread.start_new_thread(temperature_loop, (Talarms,))
 except Exception, exc:
     print("Error: unable to start thread, exception=%s" % str(exc))
 
