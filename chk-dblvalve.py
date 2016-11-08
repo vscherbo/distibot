71be8e85104d67a67e6749e2d9d3d8b0f6eb5238
Vladimir Scherbo <vscherbo@gmail.com>
2016-11-08 22:32:28 +0300

    проверка двойного клапана, pre-alpha

--- chk-dblvalve.py
+++ chk-dblvalve.py
@@ -1,41 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+log_format = '%(levelname)s | %(asctime)-15s | %(message)s'
+logging.basicConfig(format=log_format, level=logging.DEBUG)
+import RPIO
 import time
 import valve
-import RPIO
 import sys
 
 RPIO.setmode(RPIO.BCM)
-#res = RPIO.gpio_function(10)
-#print "res=",str(res)
-#sys.exit()
+# res = RPIO.gpio_function(10)
+# print "res=",str(res)
+# sys.exit()
 
-v1 = valve.DoubleValve(gpio_1_2 = 23)
-print('default')
-v1.default_way()
-time.sleep(3)
+v1 = valve.DoubleValve(gpio_1_2=23)
 
-print('power_on')
-v1.power_on_way()
-time.sleep(3)
+
+def socket_callback(socket, val):
+    print("socket %s: '%s'" % (socket.fileno(), val))
+    global do_flag
+    do_flag = False
+    socket.send("echo: %s\n" % val)
+    global v1
+    if 1 == val:
+        v1.way_1()
+    elif 2 == val:
+        v1.way_2()
+    elif 3 == val:
+        v1.way_3()
+    else:
+        print("unknown valve way=%s" % val)
+    # RPIO.close_tcp_client(socket.fileno())
+
+# TCP socket server callback on port 8080
+RPIO.add_tcp_callback(8080, socket_callback, threaded_callback=True)
+
+# Blocking main epoll loop
+RPIO.wait_for_interrupts(threaded=True)
+
+do_flag = True
+
+# main loop
+while do_flag:
+    print "wait for cmd on port 8080"
+    time.sleep(2)
+
 
 v1.release()
-
-RPIO.cleanup()
 sys.exit()
-
-
-
-#v1.way_1()
-#v1.way_2()
-#v1.way_1()
-
-"""
-v1.v1_turn_off()
-time.sleep(3)
-v1.v1_turn_on()
-time.sleep(3)
-#v1.v1_turn_off()
-"""
-
