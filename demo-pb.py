c61d61d69ec6bd2579cadf2dad6c26f0864579ff
Vladimir Scherbo <vscherbo@gmail.com>
2016-11-13 21:01:46 +0300

    Demo pushbullet channel

--- demo-pb.py
+++ demo-pb.py
@@ -4,11 +4,13 @@
 from pushbullet import Pushbullet
 
 pb = Pushbullet('XmJ61j9LVdjbPyKcSOUYv1k053raCeJP')
+c = [x for x in pb.channels if x.name == u"Billy's moonshine"][0]
+c.push_note("Заголовок", "Сообщение")
 
 # push = pb.push_note("This is the title", "This is the body")
 # print(pb.devices)
 
-one_plus_one = pb.get_device('OnePlus One')
+# one_plus_one = pb.get_device('OnePlus One')
+# push = one_plus_one.push_note("Процесс", "Тело.")
 
-push = one_plus_one.push_note("Процесс", "Тело.")
-# push = pb.push_note("Hello world!", "We're using the api.", device=one_plus_one)
+
