e40847233ef455ec705c12605671d84b45508e13
Vladimir Scherbo <vscherbo@gmail.com>
2016-11-19 12:30:37 +0300

    форматирование

--- tsensor.py
+++ tsensor.py
@@ -3,10 +3,10 @@
 
 from __future__ import print_function
 import w1thermsensor
-from time import strftime
 
-class tsensor(w1thermsensor.W1ThermSensor):
-    def __init__(self): # TODO name or id of sensor
+
+class Tsensor(w1thermsensor.W1ThermSensor):
+    def __init__(self):  # TODO name or id of sensor
         try:
             # sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "0000066c6502")
             # ll /sys/bus/w1/devices/
@@ -14,7 +14,7 @@ class tsensor(w1thermsensor.W1ThermSensor):
             self.emu_mode = False
         except w1thermsensor.core.KernelModuleLoadError:
             self.emu_mode = True
-            self.Trange=[x * 0.1 for x in range(200, 99999)]
+            self.Trange = [x * 0.1 for x in range(200, 99999)]
             self.emu_iterator = iter(self.Trange)
 
     def get_temperature(self, unit=w1thermsensor.W1ThermSensor.DEGREES_C):
