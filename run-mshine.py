d81bf5be1cef1f1c74aad222116203bde7ff420c
Vladimir Scherbo <vscherbo@gmail.com>
2016-11-19 12:32:32 +0300

    добавлена bottle, pre-alpha

--- run-mshine.py
+++ run-mshine.py
@@ -3,10 +3,12 @@
 
 from __future__ import print_function
 from mshinectl import Moonshine_controller
-import signal
 import sys
-import time
+import socket
+import signal
 import thread
+from bottle import route, run, debug, template, static_file, get
+# request, post
 
 
 def signal_handler(signal, frame):
@@ -30,10 +32,40 @@ except Exception, exc:
     print("Error: unable to start thread, exception=%s" % str(exc))
 
 
-do_flag = True
-while do_flag:
-    time.sleep(5)
-    pass
+# Static Routes
+@get('/<filename:re:.*\.js>')
+def javascripts(filename):
+        return static_file(filename, root='static/js')
+
+
+@get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='static/css')
+
+"""
+@get('/<filename:re:.*\.png>')
+def stylesheets(filename):
+    return static_file(filename, root='static/images')
+"""
+
+
+# it works
+@route('/tsensor')
+def t_show():
+    output = template('temperature_show')
+    return output
+
+
+@route('/ask_t')
+def ask_temperature():
+    curr_temperature = mshinectl.sensor.get_temperature()
+    return str(curr_temperature)
+
+# add this at the very end:
+debug(True)
+
+loc_host = socket.gethostbyname(socket.gethostname())
+run(host=loc_host, reloader=True)
 
 print("Exiting!")
 sys.exit(0)
