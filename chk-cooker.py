bff2f4c7e3a0491b4be1ad32638b8f2ff9a099a1
Vladimir Scherbo <vscherbo@gmail.com>
2016-12-24 18:40:19 +0300

    вызов switch_off и RPIO.cleanup заменён на release

--- chk-cooker.py
+++ chk-cooker.py
@@ -4,7 +4,7 @@ import cooker
 from time import sleep
 import RPIO
 
-ck = cooker.Cooker(gpio_on_off= 17, gpio_up = 22, gpio_down = 27)
+ck = cooker.Cooker(gpio_on_off=17, gpio_up=22, gpio_down=27)
 #ck = cooker.Cooker(17,22,27)
 ck.switch_on()
 sleep(2)
@@ -25,6 +25,6 @@ sleep(2)
 print ck.current_power()
 sleep(5)
 
-ck.switch_off()
+ck.release()
 
-RPIO.cleanup()
+# RPIO.cleanup()
