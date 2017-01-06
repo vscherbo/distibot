15e8667e31911ee979b2bed3eb3a945a0187447c
Vladimir Scherbo <vscherbo@gmail.com>
2017-01-06 17:58:03 +0300

    в тест добавлен gpio_fry

--- chk-cooker.py
+++ chk-cooker.py
@@ -3,7 +3,7 @@
 import cooker
 from time import sleep
 
-ck = cooker.Cooker(gpio_on_off=17, gpio_up=22, gpio_down=27)
+ck = cooker.Cooker(gpio_on_off=17, gpio_up=22, gpio_down=27, gpio_fry=15)
 ck.switch_on()
 sleep(2)
 ck.power_up()
@@ -22,6 +22,11 @@ ck.power_down()
 sleep(2)
 print ck.current_power()
 sleep(5)
+
+print('set Fry mode')
+ck.set_fry()
+sleep(2)
+print ck.current_power()
 
 ck.release()
 
