c914041133abbdcb2068c32ce25cc4bd12e3f098
Vladimir Scherbo <vscherbo@gmail.com>
2017-04-30 20:06:20 +0300

    добавлено измерение скорости потока (alpha)

--- flow_sensor.py
+++ flow_sensor.py
@@ -6,6 +6,7 @@ log_format = '%(levelname)s | %(asctime)-15s | %(message)s'
 logging.basicConfig(format=log_format, level=logging.INFO)
 # import RPIO_wrap.RPIO as RPIO
 import RPIO
+import time
 logger = logging.getLogger(__name__)
 # hs_handler = logging.FileHandler('moonshine.log')
 # hs_handler = logging.StreamHandler()
@@ -15,8 +16,30 @@ logger = logging.getLogger(__name__)
 
 
 class Flow_sensor:
+    PINTS_IN_A_LITER = 2.11338
+    SECONDS_IN_A_MINUTE = 60
+    MS_IN_A_SECOND = 1000.0
+    displayFormat = 'metric'
+    beverage = 'beer'
+    enabled = True
+    clicks = 0
+    lastClick = 0
+    clickDelta = 0
+    hertz = 0.0
+    flow = 0 # in Liters per second
+    thisPour = 0.0 # in Liters
+    totalPour = 0.0 # in Liters
+    count = 0
 
     def __init__(self, gpio_flow_pin):
+        self.clicks = 0
+        self.lastClick = int(time.time() * self.MS_IN_A_SECOND)
+        self.clickDelta = 0
+        self.hertz = 0.0
+        self.flow = 0.0
+        self.thisPour = 0.0
+        self.totalPour = 0.0
+        self.enabled = True
         self.gpio_flow_pin = gpio_flow_pin
         logger.info('init flow-sensor GPIO_flow={0}'.format(self.gpio_flow_pin))
 
@@ -33,19 +56,31 @@ class Flow_sensor:
 #                                    debounce_timeout_ms=self.timeout,
                                     pull_up_down=RPIO.PUD_UP)
 
+    def update(self, currentTime):
+        self.clicks += 1
+        # get the time delta
+        self.clickDelta = max((currentTime - self.lastClick), 1)
+        # calculate the instantaneous speed
+        if (self.enabled == True and self.clickDelta < 1000):
+          self.hertz = self.MS_IN_A_SECOND / self.clickDelta
+          self.flow = self.hertz / (self.SECONDS_IN_A_MINUTE * 8.1)  # In Liters per second
+          instPour = self.flow * (self.clickDelta / self.MS_IN_A_SECOND)  
+          self.thisPour += instPour
+          self.totalPour += instPour
+        # Update the last click
+        self.lastClick = currentTime
 
-def countPulse(gpio_id, value):
-   global count
-   count = count+1
-   print count
+    def countPulse(self, gpio_id, value):
+       self.count = self.count+1
+       currentTime = int(time.time() * self.MS_IN_A_SECOND)
+       self.update(currentTime)
+       print "Count={0} Pour={1}".format(self.count, self.thisPour)
 
 if __name__ == "__main__":
     import time
     import sys
-    global count
-    count = 0
     fs = Flow_sensor(7)
-    fs.watch_flow(countPulse)
+    fs.watch_flow(fs.countPulse)
     while True:
         try:
             time.sleep(1)
