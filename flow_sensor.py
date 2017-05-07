2d9f2626b72d02a85836aeb5bcc6a327d5dd0937
Vladimir Scherbo <vscherbo@gmail.com>
2017-05-07 18:07:11 +0300

    переход на RPi.GPIO

--- flow_sensor.py
+++ flow_sensor.py
@@ -4,8 +4,8 @@
 import logging
 log_format = '%(levelname)s | %(asctime)-15s | %(message)s'
 logging.basicConfig(format=log_format, level=logging.INFO)
-# import RPIO_wrap.RPIO as RPIO
-import RPIO
+# import GPIO_wrap.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 logger = logging.getLogger(__name__)
 # hs_handler = logging.FileHandler('moonshine.log')
@@ -27,8 +27,8 @@ class Flow_sensor:
     clickDelta = 0
     hertz = 0.0
     flow = 0 # in Liters per second
+    instPour = 0.0 # current flow
     thisPour = 0.0 # in Liters
-    totalPour = 0.0 # in Liters
     count = 0
 
     def __init__(self, gpio_flow_pin):
@@ -44,17 +44,13 @@ class Flow_sensor:
         logger.info('init flow-sensor GPIO_flow={0}'.format(self.gpio_flow_pin))
 
     def release(self):
-        RPIO.setup(self.gpio_flow_pin, RPIO.OUT)
+        GPIO.setup(self.gpio_flow_pin, GPIO.OUT)
         print "flow_sensor released"
 
     def watch_flow(self, flow_callback):
-        RPIO.setup(self.gpio_flow_pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
-        RPIO.wait_for_interrupts(threaded=True)
-        RPIO.add_interrupt_callback(self.gpio_flow_pin,
-                                    flow_callback,
-                                    edge='falling',
-#                                    debounce_timeout_ms=self.timeout,
-                                    pull_up_down=RPIO.PUD_UP)
+        GPIO.setup(self.gpio_flow_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.gpio_flow_pin, GPIO.FALLING)
+        GPIO.add_event_callback(self.gpio_flow_pin, callback=flow_callback)
 
     def update(self, currentTime):
         self.clicks += 1
@@ -63,23 +59,26 @@ class Flow_sensor:
         # calculate the instantaneous speed
         if (self.enabled == True and self.clickDelta < 1000):
           self.hertz = self.MS_IN_A_SECOND / self.clickDelta
-          self.flow = self.hertz / (self.SECONDS_IN_A_MINUTE * 8.1)  # In Liters per second
-          instPour = self.flow * (self.clickDelta / self.MS_IN_A_SECOND)  
-          self.thisPour += instPour
-          self.totalPour += instPour
+          self.myFlow = self.hertz /700
+          # self.flow = self.hertz / (self.SECONDS_IN_A_MINUTE * 12.0)  # In Liters per second
+          # instPour = self.flow * (self.clickDelta / self.MS_IN_A_SECOND)  
+          self.instPour = self.myFlow * (self.clickDelta / self.MS_IN_A_SECOND)  
+          self.thisPour += self.instPour
         # Update the last click
         self.lastClick = currentTime
 
-    def countPulse(self, gpio_id, value):
+    def countPulse(self, gpio_id):
        self.count = self.count+1
        currentTime = int(time.time() * self.MS_IN_A_SECOND)
        self.update(currentTime)
-       print "Count={0} Pour={1}".format(self.count, self.thisPour)
+       print "Count={0} Pour={1}".format(self.clicks, self.thisPour)
 
 if __name__ == "__main__":
     import time
     import sys
-    fs = Flow_sensor(7)
+    GPIO.setmode(GPIO.BCM)
+    channel=7
+    fs = Flow_sensor(channel)
     fs.watch_flow(fs.countPulse)
     while True:
         try:
@@ -87,6 +86,6 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             print '\ncaught keyboard interrupt!, bye'
             fs.release()
-            RPIO.cleanup()
+            GPIO.remove_event_detect(channel)
             sys.exit()
 
