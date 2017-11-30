e06de482bc9729a49055764d21978f47b06fca8c
Vladimir Scherbo <vscherbo@gmail.com>
2017-11-30 11:40:23 +0300

    using FakeRPi; logging instead of logger

--- cooker.py
+++ cooker.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import RPi.GPIO as GPIO
-import gpio_dev
+# import gpio_dev
+from gpio_dev import *
 import time
+import logging
 
-
-class Cooker(gpio_dev.GPIO_DEV):
+class Cooker(GPIO_DEV):
     powers = (120, 300, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000)
     max_power_index = len(powers)-1
     power_max = powers[-1]
@@ -35,7 +35,7 @@ class Cooker(gpio_dev.GPIO_DEV):
         self.target_power_index = 0
 
     def release(self):
-        self.logger.info("cooker.release")
+        logging.info("cooker.release")
         self.switch_off()
         super(Cooker, self).release()
 
@@ -45,7 +45,7 @@ class Cooker(gpio_dev.GPIO_DEV):
         GPIO.output(gpio_port_num, 1)
         time.sleep(0.1)
         GPIO.output(gpio_port_num, 0)
-        self.logger.debug('clicked GPIO_port={gpio}'.format(gpio=gpio_port_num))
+        logging.debug('clicked self_port={gpio}'.format(gpio=gpio_port_num))
 
     def switch_on(self, force_mode=False):
         if force_mode:
@@ -59,12 +59,12 @@ class Cooker(gpio_dev.GPIO_DEV):
         if force_mode:
             self.state_on = True
         if self.state_on:
-            self.logger.info("switch_OFF")
+            logging.info("switch_OFF")
             self.click_button(self.gpio_on_off)
             self.state_on = False
 
     def set_fry(self):
-        self.logger.info("set_Fry")
+        logging.info("set_Fry")
         self.click_button(self.gpio_fry)
         self.power_index = self.max_power_index
 
@@ -72,37 +72,37 @@ class Cooker(gpio_dev.GPIO_DEV):
         if self.power_index < self.max_power_index:
             self.click_button(self.gpio_up)
             self.power_index += 1
-            self.logger.debug("power_up, new_index={}".format(self.power_index))
+            logging.debug("power_up, new_index={}".format(self.power_index))
             return True
         else:
-            self.logger.debug("power_up False, index={}".format(self.power_index))
+            logging.debug("power_up False, index={}".format(self.power_index))
             return False
 
     def set_power_max(self):
         while self.power_up():
             pass
-            self.logger.debug("set_power_max loop, power={}".format(self.current_power()))
+            logging.debug("set_power_max loop, power={}".format(self.current_power()))
 
     def power_down(self):
         if self.power_index > 0:
             self.click_button(self.gpio_down)
             self.power_index -= 1
-            self.logger.debug("power_down, new_index={}".format(self.power_index))
+            logging.debug("power_down, new_index={}".format(self.power_index))
             return True
         else:
-            self.logger.debug("power_down False, index={}".format(self.power_index))
+            logging.debug("power_down False, index={}".format(self.power_index))
             return False
 
     def set_power_min(self):
         while self.power_down():
             pass
-            self.logger.debug("set_power_min loop, power={}".format(self.current_power()))
+            logging.debug("set_power_min loop, power={}".format(self.current_power()))
 
     def set_power_600(self):
         self.switch_on()
         while self.current_power() > 600:
             self.power_down()
-            self.logger.debug("power_600 loop, power={}".format(self.current_power()))
+            logging.debug("power_600 loop, power={}".format(self.current_power()))
 
     def set_power_300(self):
         self.set_power(300)
@@ -117,7 +117,8 @@ class Cooker(gpio_dev.GPIO_DEV):
         # TODO detect wrong power OR approximate
         try:
             self.target_power_index = self.powers.index(power)
-        except LookupError as e:
+        except LookupError:
+            logging.error('set_power index lookup', exc_info=True)
             self.switch_off()
             self.switch_on()
         else:
@@ -133,22 +134,31 @@ class Cooker(gpio_dev.GPIO_DEV):
 
 if __name__ == '__main__':
     from time import sleep
+    import os
+    log_dir = ''
+    log_format = '[%(filename)-20s:%(lineno)4s - %(funcName)20s()] %(levelname)-7s | %(asctime)-15s | %(message)s'
+
+    (prg_name, prg_ext) = os.path.splitext(os.path.basename(__file__))
+    logging.basicConfig(filename=prg_name+'.log', format=log_format, level=logging.INFO)
+
+    logging.info('Started')
+
     ck = Cooker(gpio_on_off=17, gpio_up=22, gpio_down=27, gpio_fry=15)
     ck.switch_on()
     sleep(2)
 
     ck.set_power_1800()
-    ck.logger.info(ck.current_power())
+    logging.info(ck.current_power())
     sleep(3)
 
     ck.set_power_1200()
-    ck.logger.info(ck.current_power())
+    logging.info(ck.current_power())
     sleep(3)
 
-    ck.logger.info('set Fry mode')
     ck.set_fry()
     sleep(2)
-    ck.logger.info(ck.current_power())
+    logging.info(ck.current_power())
 
     ck.release()
 
+    logging.info('Finished')
--- gpio_dev.py
+++ gpio_dev.py
@@ -1,32 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import RPi.GPIO as GPIO
-import logging
-log_format = '%(levelname)-7s | %(asctime)-15s | %(message)s'
-logging.basicConfig(format=log_format, level=logging.DEBUG)
+"""
+Class for GPIO output devices
+"""
 
+
+"""
+import importlib.util
+try:
+    importlib.util.find_spec('RPi.GPIO')
+    import RPi.GPIO as GPIO
+"""
+
+import imp
+try:
+    imp.find_module('RPi.GPIO')
+    import RPi.GPIO as GPIO
+except ImportError:
+    """
+    import FakeRPi.GPIO as GPIO
+    OR
+    import FakeRPi.RPiO as RPiO
+    """
+    import FakeRPi.GPIO as GPIO
+
+import logging
 
 class GPIO_DEV(object):
-    log_dir = 'log/'
 
     def __init__(self):
         self.gpio_list = []
-        self.logger = logging.getLogger("gpio")
-        if not len(self.logger.handlers):
-            file_handler = logging.FileHandler(self.log_dir + 'gpio.log')
-            formatter = logging.Formatter(log_format)
-            file_handler.setFormatter(formatter)
-            self.logger.addHandler(file_handler)
-            self.logger.setLevel(logging.DEBUG)
+        logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-        mode = GPIO.getmode()
-        if mode != GPIO.BCM or mode is None:
-            GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM)
+        logging.info('gpio_class initialized')
 
     def release(self):
         if self.gpio_list is not None:
             GPIO.cleanup(self.gpio_list)
             s = "cleaned gpio_list=[" + ', '.join(['{}']*len(self.gpio_list)) + "]"
-            self.logger.info(s.format(*self.gpio_list))
-            self.logger.info('gpio_class released')
+            logging.info(s.format(*self.gpio_list))
+            logging.info('gpio_class released')
+
+
+if __name__ == "__main__":
+    import os
+    log_dir = ''
+    log_format = '[%(filename)-20s:%(lineno)4s - %(funcName)20s()] %(levelname)-7s | %(asctime)-15s | %(message)s'
+
+    (prg_name, prg_ext) = os.path.splitext(os.path.basename(__file__))
+    logging.basicConfig(filename=prg_name+'.log', format=log_format, level=logging.INFO)
+
+    logging.info('Started')
+    g1 = GPIO_DEV()
+    g1.release()
+    logging.info('Finished')
+
