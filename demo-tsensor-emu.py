33b976f77ddc896ecafb4f0425d7c4d57b156da0
Vladimir Scherbo <vscherbo@gmail.com>
2017-12-03 17:31:35 +0300

    развитие demo

--- demo-tsensor-emu.py
+++ demo-tsensor-emu.py
@@ -6,11 +6,22 @@ import time
 
 t1 = tsensor.Tsensor()
 
-cnt = 0
-T = 0
-# while cnt < 3:
-while T < 99.9:
-    T = t1.get_temperature()
-    print T
+t_range = (20, 21, 22, -200, 23, 24, 25, 111, 26, 27)
+
+t_prev = t_range[0]
+print 't_prev={}'.format(t_prev)
+
+for t in t_range:
+    if abs((t-t_prev)/t_prev) > 0.3:
+        t_curr = t_prev
+    else:
+        t_prev = t
+        t_curr = t
+    print t_curr
+
+
+cnt = 4
+while cnt < 3:
+    print t1.get_temperature()
     cnt += 1
-    # time.sleep(1)
+    time.sleep(1)
