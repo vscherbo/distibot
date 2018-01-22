dadd2465a8182fede0fa8bb05b31f359dcc7b882
Vladimir Scherbo <vscherbo@gmail.com>
2018-01-22 16:49:49 +0300

    pre-alpha: сценарий для множеств температурных датчиков

--- demo-dict-from-file.py
+++ demo-dict-from-file.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import collections
-import time
+# import time
+import sys
 
 class Moonshine_controller(object):
 
@@ -12,9 +13,10 @@ class Moonshine_controller(object):
 
     def load_config(self, conf_file_name):
         conf = open(conf_file_name, 'r')
-        self.Tsteps = collections.OrderedDict(sorted(eval(conf.read()).items(), key=lambda t: t[0]))
+        # self.Tsteps = collections.OrderedDict(sorted(eval(conf.read()).items(), key=lambda t: t[0]))
+        self.conf_dict = collections.OrderedDict(eval(conf.read()).items())
         conf.close()
-        self.set_Tsteps(self.Tsteps)
+        # self.set_Tsteps(self.Tsteps)
 
     def set_Tsteps(self, Tsteps):
         self.Tkeys = Tsteps.keys()
@@ -30,11 +32,26 @@ class Moonshine_controller(object):
     def stop_body(self):
         print "stop_body"
 
+    def heat_on_pause(self):
+        print "heat_on_pause"
+
+    def heat_4_low_wine(self):
+        print "heat_4_low_wine"
+
+    def start_watch_temperature(self):
+        print "start_watch_temperature"
+
     def finish(self):
         print "finish"
 
     def do_nothing(self):
         print "do_nothing"
+
+    def emergency_finish(self):
+        print "emergency_finish"
+
+    def send_msg(self):
+        print "send_msg"
 
 mshinectl = Moonshine_controller(None)
 
@@ -46,7 +63,16 @@ inf = open('myfile.txt','r')
 Tsteps = collections.OrderedDict(sorted(eval(inf.read()).items(), key=lambda t: t[0]))
 inf.close()
 """
-mshinectl.load_config('myfile.txt')
+mshinectl.load_config('dib-test.play2')
+
+print "=== conf_dict"
+print len(mshinectl.conf_dict)
+print mshinectl.conf_dict.keys()
+
+for key,val in mshinectl.conf_dict.iteritems():
+    print 'key={0}, val={1}'.format(key, val)
+
+sys.exit(0)
 
 print "=== Tsteps"
 print mshinectl.Tsteps
