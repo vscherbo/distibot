f0b73e10ba01b2e083181e71c2dac7866f030d99
Vladimir Scherbo <vscherbo@gmail.com>
2018-01-22 17:36:28 +0300

    разбор конфигурационного файла; добавлен аргумент тип датчика уровня OPT/RES; stop заменён на finish

--- heads_sensor.py
+++ heads_sensor.py
@@ -7,25 +7,36 @@ import logging
 
 class Heads_sensor(GPIO_DEV):
 
-    def __init__(self, gpio_heads_start, gpio_heads_stop, timeout=1000):
+    def __init__(self, hs_type, gpio_heads_start, gpio_heads_finish, timeout=1000):
         super(Heads_sensor, self).__init__()
+
+        if 'OPT' == hs_type:
+            self.edge = GPIO.FALLING
+            self.PUD = GPIO.PUD_UP
+        elif 'RES' == hs_type:
+            self.edge = GPIO.RISING
+            self.PUD = GPIO.PUD_DOWN
+        else:
+            raise NameError('a wrong value=[{0}] for hs_type'.format(hs_type))
+
         self.timeout = timeout
         self.flag_ignore_start = True
-        self.flag_ignore_stop = True
+        self.flag_ignore_finish = True
         #
         self.gpio_heads_start = gpio_heads_start
         self.gpio_list.append(gpio_heads_start)
         #
-        self.gpio_heads_stop = gpio_heads_stop
-        self.gpio_list.append(gpio_heads_stop)
+        self.gpio_heads_finish = gpio_heads_finish
+        self.gpio_list.append(gpio_heads_finish)
         #
-        hs_gpios = {'gpio_start': gpio_heads_start,
-                    'gpio_stop': gpio_heads_stop}
-        logging.info('init heads-sensor GPIO_start={gpio_start}, GPIO_stop={gpio_stop}'.format(**hs_gpios))
+        hs_args = {'hs_type': hs_type,
+                    'gpio_start': gpio_heads_start,
+                    'gpio_finish': gpio_heads_finish}
+        logging.info('init {hs_type} heads-sensor GPIO_start={gpio_start}, GPIO_finish={gpio_finish}'.format(**hs_args))
 
     def release(self):
         self.ignore_start()
-        self.ignore_stop()
+        self.ignore_finish()
         super(Heads_sensor, self).release()
         logging.info("heads_sensor release")
 
@@ -37,38 +48,38 @@ class Heads_sensor(GPIO_DEV):
             logging.info('ignore_start: remove_event_detect on {0}'.format(self.gpio_heads_start))
             GPIO.remove_event_detect(self.gpio_heads_start)
 
-    def ignore_stop(self):
-        if self.flag_ignore_stop:
+    def ignore_finish(self):
+        if self.flag_ignore_finish:
             pass
         else:
-            self.flag_ignore_stop = True
-            logging.info('ignore_stop: remove_eventy_detect on {0}'.format(self.gpio_heads_stop))
-            GPIO.remove_event_detect(self.gpio_heads_stop)
+            self.flag_ignore_finish = True
+            logging.info('ignore_finish: remove_eventy_detect on {0}'.format(self.gpio_heads_finish))
+            GPIO.remove_event_detect(self.gpio_heads_finish)
 
-    # TODO merge watch_start & watch_stop in a single method
+    # TODO merge watch_start & watch_finish in a single method
     def watch_start(self, start_callback):
         self.flag_ignore_start = False
-        GPIO.setup(self.gpio_heads_start, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.gpio_heads_start, GPIO.RISING, bouncetime=self.timeout)
+        GPIO.setup(self.gpio_heads_start, GPIO.IN, pull_up_down=self.PUD)
+        GPIO.add_event_detect(self.gpio_heads_start, self.edge, bouncetime=self.timeout)
         GPIO.add_event_callback(self.gpio_heads_start, start_callback)
 
-    def watch_stop(self, stop_callback):
-        self.flag_ignore_stop = False
-        GPIO.setup(self.gpio_heads_stop, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.gpio_heads_stop, GPIO.RISING, bouncetime=self.timeout)
-        GPIO.add_event_callback(self.gpio_heads_stop, stop_callback)
+    def watch_finish(self, finish_callback):
+        self.flag_ignore_finish = False
+        GPIO.setup(self.gpio_heads_finish, GPIO.IN, pull_up_down=self.PUD)
+        GPIO.add_event_detect(self.gpio_heads_finish, self.edge, bouncetime=self.timeout)
+        GPIO.add_event_callback(self.gpio_heads_finish, finish_callback)
 
 if __name__ == "__main__":
     import time
+    import argparse
     import signal
     import os
-    log_dir = ''
-    log_format = '[%(filename)-20s:%(lineno)4s - %(funcName)20s()] %(levelname)-7s | %(asctime)-15s | %(message)s'
-
-    (prg_name, prg_ext) = os.path.splitext(os.path.basename(__file__))
-    logging.basicConfig(filename=prg_name+'.log', format=log_format, level=logging.INFO)
+    import sys
+    import ConfigParser
+    import io
 
     def signal_handler(signal, frame):
+
         global loop_flag
         loop_flag = False
 
@@ -77,15 +88,52 @@ if __name__ == "__main__":
     def heads_started(gpio_id):
         global hs
         logging.info("{} Стартовали головы, gpio_id={}".format(time.strftime("%Y-%m-%d-%H-%M-%S"), gpio_id))
-        hs.watch_stop(heads_finished)
+        hs.watch_finish(heads_finished)
         hs.ignore_start()
 
     def heads_finished(gpio_id):
         global hs
         logging.info("{} Закончились головы, gpio_id={}".format(time.strftime("%Y-%m-%d-%H-%M-%S"), gpio_id))
-        hs.ignore_stop()
+        hs.ignore_finish()
 
-    hs = Heads_sensor(gpio_heads_start=25, gpio_heads_stop=14,
+    log_dir = ''
+    log_format = '[%(filename)-20s:%(lineno)4s - %(funcName)20s()] %(levelname)-7s | %(asctime)-15s | %(message)s'
+
+    (prg_name, prg_ext) = os.path.splitext(os.path.basename(__file__))
+    conf_file_name = "distibot.conf"
+
+    parser = argparse.ArgumentParser(description='Distibot "tsensor" module')
+    parser.add_argument('--conf', type=str, default=conf_file_name, help='conf file')
+    parser.add_argument('--log_to_file', type=bool, default=False, help='log destination')
+    parser.add_argument('--log_level', type=str, default="DEBUG", help='log level')
+    args = parser.parse_args()
+
+    numeric_level = getattr(logging, args.log_level, None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % numeric_level)
+
+    # log_format = '[%(filename)-20s:%(lineno)4s - %(funcName)20s()] %(levelname)-7s | %(asctime)-15s | %(message)s'
+    log_format = '%(asctime)-15s | %(levelname)-7s | %(message)s'
+
+    if args.log_to_file:
+        log_dir = ''
+        log_file = log_dir + prg_name + ".log"
+        logging.basicConfig(filename=log_file, format=log_format, level=numeric_level)
+    else:
+        logging.basicConfig(stream=sys.stdout, format=log_format, level=numeric_level)
+
+    logging.info('Started')
+
+    with open(args.conf) as f:
+        dib_config = f.read()
+        f.close()
+
+    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    config.readfp(io.BytesIO(dib_config))
+
+    hs = Heads_sensor(hs_type=config.get('heads_sensor', 'hs_type'),
+                      gpio_heads_start=config.getint('heads_sensor', 'gpio_hs_start'),
+                      gpio_heads_finish=config.getint('heads_sensor', 'gpio_hs_finish'),
                       timeout=2000)
     hs.watch_start(heads_started),
 
@@ -93,7 +141,7 @@ if __name__ == "__main__":
     step_counter = 0
     while loop_flag:
         step_counter += 1
-        logging.info("step={}".format(step_counter))
+        logging.info("step={step:>4}".format(step=step_counter))
         time.sleep(2)
 
     hs.release()
