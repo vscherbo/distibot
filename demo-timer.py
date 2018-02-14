0582615a5367d5712304a16d4b959e5c38a3230a
Vladimir Scherbo <vscherbo@gmail.com>
2018-02-14 17:19:11 +0300

    working demo-timer

--- demo-timer.py
+++ demo-timer.py
@@ -7,10 +7,6 @@ import signal
 import time
 
 
-def hello():
-    print " world"
-
-
 class Cooker_stub(object):
 
     def __init__(self):
@@ -28,25 +24,29 @@ class SomeClass(object):
     def __init__(self):
         logging.getLogger(__name__).addHandler(logging.NullHandler())
         self.cooker = Cooker_stub()
-        self.cooker_period = 10
+        self.cooker_period = 5
         self.cooker_timeout = 2
-        # self.cooker_timer = threading.Timer(self.cooker_period, self.cooker_off)
-        self.cooker_timer = threading.Timer(self.cooker_period, hello)
-        logging.debug(self.cooker_timer.interval)
 
     def cooker_off(self):
         self.cooker.switch_off()
         self.cooker_pause_timer = threading.Timer(self.cooker_timeout, self.cooker_on)
+        self.cooker_pause_timer.start()
 
     def cooker_on(self):
         self.cooker.switch_on()
         self.cooker_timer = threading.Timer(self.cooker_period, self.cooker_off)
+        self.cooker_timer.start()
 
 if __name__ == "__main__":
     import os
 
     def signal_handler(signal, frame):
         global flag_do
+        global o
+        if o.cooker_timer:
+            o.cooker_timer.cancel()
+        if o.cooker_pause_timer:
+            o.cooker_pause_timer.cancel()
         flag_do = False
 
     signal.signal(signal.SIGINT, signal_handler)
@@ -61,10 +61,14 @@ if __name__ == "__main__":
 
     logging.info('Started')
     o = SomeClass()
+    o.cooker_on()
+    # loc_timer = threading.Timer(5, hello)
     flag_do = True
     while flag_do:
         time.sleep(1)
-        is_alive = o.cooker_timer.is_alive()
-        logging.debug('loop, cooker_timer.is_alive={0}'.format(is_alive))
+        #is_alive = o.cooker_timer.is_alive()
+        #pause_is_alive = o.cooker_pause_timer.is_alive()
+        #logging.debug('loop, is_alive={0}, pause_is_alive={1}'.format(is_alive, pause_is_alive))
+        logging.debug('loop')
 
     logging.info('Finished')
