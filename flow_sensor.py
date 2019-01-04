e128063e614458c5679022428de090286826e6a8
Vladimir Scherbo <vscherbo@gmail.com>
2019-01-04 12:12:09 +0300

    удалён флаг enabled

--- flow_sensor.py
+++ flow_sensor.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from gpio_dev import GPIO_DEV, GPIO
 import time
 import logging
+from gpio_dev import GPIO_DEV, GPIO
 
 
 class Flow_sensor(GPIO_DEV):
     SECONDS_IN_A_MINUTE = 60
     MS_IN_A_SECOND = 1000.0
-    enabled = True
     clicks = 0
     lastClick = 0
     clickDelta = 0
@@ -27,7 +26,6 @@ class Flow_sensor(GPIO_DEV):
         self.flow = 0.0
         self.thisPour = 0.0
         self.totalPour = 0.0
-        self.enabled = True
         self.gpio_fs = gpio_fs
         self.gpio_list.append(gpio_fs)
         logging.info('init flow-sensor GPIO_flow={0}'.format(self.gpio_fs))
@@ -35,7 +33,7 @@ class Flow_sensor(GPIO_DEV):
     def release(self):
         GPIO.remove_event_detect(self.gpio_fs)
         super(Flow_sensor, self).release()
-        logging.debug("flow_sensor released")
+        logging.info("flow_sensor released")
 
     def watch_flow(self, flow_callback):
         GPIO.setup(self.gpio_fs, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -48,11 +46,10 @@ class Flow_sensor(GPIO_DEV):
         # get the time delta
         self.clickDelta = max((currentTime - self.lastClick), 1)
         # calculate the instantaneous speed
-        if self.enabled is True:
-            self.hertz = self.MS_IN_A_SECOND / self.clickDelta
-            self.flow = self.hertz / 700
-            self.instPour = self.flow * (self.clickDelta / self.MS_IN_A_SECOND)
-            self.thisPour += self.instPour
+        self.hertz = self.MS_IN_A_SECOND / self.clickDelta
+        self.flow = self.hertz / 700
+        self.instPour = self.flow * (self.clickDelta / self.MS_IN_A_SECOND)
+        self.thisPour += self.instPour
         # Update the last click
         self.lastClick = currentTime
 
@@ -63,8 +60,8 @@ if __name__ == "__main__":
 
     log_format = '%(asctime)-15s | %(levelname)-7s | %(message)s'
     numeric_level = logging.DEBUG
-    logging.basicConfig(stream=sys.stdout, format=log_format, level=numeric_level)
-
+    logging.basicConfig(stream=sys.stdout, format=log_format,
+                        level=numeric_level)
 
     class FS_tester():
 
@@ -83,7 +80,8 @@ if __name__ == "__main__":
             self.do_flag = False
 
         def no_flow(self):
-            logging.warning('no flow detected after flow_period={}, exiting'.format(self.flow_period))
+            logging.warning('no flow detected after flow_period={}, exiting'.
+                            format(self.flow_period))
             self.release()
 
         def flow_detected(self, gpio_id):
@@ -95,8 +93,10 @@ if __name__ == "__main__":
             self.flow_timer.start()
 
             self.flow_sensor.handle_click()
-            logging.debug("flow_count={0} V={1} Pour={2}".format(self.flow_sensor.clicks, self.flow_sensor.flow*3600, self.flow_sensor.thisPour))
-
+            logging.debug("flow_count={0} V={1} Pour={2}".format(
+                          self.flow_sensor.clicks,
+                          self.flow_sensor.flow*3600,
+                          self.flow_sensor.thisPour))
 
     gpio_fs = 11
     fst = FS_tester(gpio_fs, 5)
@@ -106,8 +106,9 @@ if __name__ == "__main__":
     while fst.do_flag:
         try:
             time.sleep(2)
-            logging.debug('timer.is_alive={}'.format(fst.flow_timer.is_alive()))
+            logging.debug('timer.is_alive={}'.
+                          format(fst.flow_timer.is_alive()))
         except KeyboardInterrupt:
-            print '\ncaught keyboard interrupt!, bye'
+            print('\ncaught keyboard interrupt!, bye')
             fst.release()
             sys.exit()
